c62058361a905be3f34132692b9742a439278dec
Jmhano <jmhano@gmail.com>
2022-10-25 18:45:41 -0400

    added handlebars functionality

--- config/connection.js
+++ config/connection.js
@@ -2,31 +2,21 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-// create connection to our db
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "127.0.0.1",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+let sequelize;
 
-// const sequelize = new Sequelize(
-//   "just_tech_news",
-//   "root",
-//   "",
-//   {
-//     host: "localhost",
-//     dialect: "mysql",
-//     socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-//   },
-//   function (err, data) {
-//     if (err) console.log(err);
-//     console.log(data);
-//   }
-// );
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
